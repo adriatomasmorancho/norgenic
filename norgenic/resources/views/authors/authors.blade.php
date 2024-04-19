2fb113e3aaa8066552cd11e452e19c9aba1b0d1c
Adria Tomas <tomas.morancho.adria@alumnat.copernic.cat>
2024-04-19 12:47:08 +0200

    Bootstrap page authors and books done

--- norgenic/resources/views/authors/authors.blade.php
+++ norgenic/resources/views/authors/authors.blade.php
@@ -7,40 +7,38 @@
 @section('content')
 @if(Session::has('message'))
 <div class="row">
-<div class="col ml-3 mt-3 alert alert-success">      
-<p>{{Session::get('message')}}</p>
-</div>
-<div class="col">
-</div>
-<div class="col">
-</div>
+    <div class="col ml-3 mt-3 alert alert-success">
+        <p>{{Session::get('message')}}</p>
+    </div>
+    <div class="col">
+    </div>
+    <div class="col">
+    </div>
 </div>
 @endif
 <div class="p-2">
     <h1>Authors</h1>
     <a class="btn btn-primary" href="{{ route('authors.create') }}">Create</a>
     <table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
             @foreach ($authors as $key => $author)
-        <tr>
-            <td>{{ $author['id'] }}</td>
-            <td>{{ $author['name'] }}</td>
-            <td>
-                <a class="btn btn-primary" href="{{ route('authors.edit', ['id' => $author->id]) }}">Edit</a>
-                <a class="btn btn-danger" href="{{ route('authors.remove', ['id' => $author->id]) }}">Remove</a>
-            </td>
-        </tr>
-                @endforeach
-
-        <!-- Puedes agregar más filas según sea necesario -->
-    </tbody>
+            <tr>
+                <td>{{ $author['id'] }}</td>
+                <td>{{ $author['name'] }}</td>
+                <td>
+                    <a class="btn btn-primary" href="{{ route('authors.edit', ['id' => $author->id]) }}">Edit</a>
+                    <a class="btn btn-danger" href="{{ route('authors.remove', ['id' => $author->id]) }}">Remove</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
     </table>
     <div>
-@endsection
+        @endsection
--- norgenic/resources/views/books/books.blade.php
+++ norgenic/resources/views/books/books.blade.php
@@ -6,11 +6,20 @@
 
 @section('content')
 @if(Session::has('message'))
-<p>{{Session::get('message')}}</p>
+<div class="row">
+    <div class="col ml-3 mt-3 alert alert-success">
+        <p>{{Session::get('message')}}</p>
+    </div>
+    <div class="col">
+    </div>
+    <div class="col">
+    </div>
+</div>
 @endif
+<div class="p-2">
     <h1>Books</h1>
-    <a href="{{ route('books.create') }}">Create</a>
-    <table>
+    <a class="btn btn-primary" href="{{ route('books.create') }}">Create</a>
+    <table class="table">
     <thead>
         <tr>
             <th>#</th>
@@ -26,11 +35,12 @@
             <td>{{ $book['title'] }}</td>
             <td>{{ $book['author'] }}</td>
             <td>
-                <a href="{{ route('books.edit', ['id' => $book->book_id]) }}">Edit</a>
-                <a href="{{ route('books.remove', ['id' => $book->book_id]) }}">Remove</a>
+                <a class="btn btn-primary" href="{{ route('books.edit', ['id' => $book->book_id]) }}">Edit</a>
+                <a class="btn btn-danger" href="{{ route('books.remove', ['id' => $book->book_id]) }}">Remove</a>
             </td>
         </tr>
                 @endforeach
     </tbody>
     </table>
+</div>
 @endsection
